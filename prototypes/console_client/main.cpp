1aa88ea0814be7018e526d7e8b96bc264b6b37a9
Mikhail Pokhikhilov <thedilletante@gmail.com>
2017-12-09 23:27:24 +0300

    Added stun request sending

--- prototypes/console_client/main.cpp
+++ prototypes/console_client/main.cpp
@@ -2,10 +2,13 @@
 #include <string>
 #include <vector>
 #include <regex>
+#include <future>
 
 #include <asio.hpp>
 
 #include <rxcpp/rx.hpp>
+
+#include <stun++/message.h>
 
 using asio::ip::udp;
 
@@ -25,9 +28,9 @@ struct udp_packet {
     udp::endpoint sender;
 };
 
-auto udp_server(uint16_t port, size_t max_packet_length) {
+auto udp_server(uint16_t port, size_t max_packet_length, std::promise<udp::socket*>& opened_socket_promise) {
     return rxcpp::observable<>::create<udp_packet>(
-        [port, max_packet_length] (rxcpp::subscriber<udp_packet> subscriber) {
+        [port, max_packet_length, &opened_socket_promise] (rxcpp::subscriber<udp_packet> subscriber) {
             // setup the environment
             std::vector<uint8_t> data(max_packet_length);
             asio::io_service service;
@@ -36,6 +39,8 @@ auto udp_server(uint16_t port, size_t max_packet_length) {
             asio::socket_base::reuse_address reuse_address(true);
             socket.set_option(reuse_address);
             socket.bind(udp::endpoint(proto, port));
+
+            opened_socket_promise.set_value(&socket);
 
             udp::endpoint sender_endpoint;
 
@@ -96,12 +101,10 @@ int main(int argc, char* argv[]) {
 
     const uint16_t port = std::atoi(argv[1]);
     const size_t max_packet_length = 1024;
-
-    asio::io_service service;
-    udp::socket socket(service, udp::v4());
-    auto subscription = integra::udp_server(port, max_packet_length)
+    std::promise<udp::socket*> socket_promise;
+    auto subscription = integra::udp_server(port, max_packet_length, socket_promise)
         .subscribe_on(rxcpp::observe_on_new_thread())
-        .subscribe([&service, &socket](integra::udp_packet msg){
+        .subscribe([](integra::udp_packet msg){
             std::cout << "Received from: "
                       << msg.sender.address().to_string()
                       << ":" << msg.sender.port()
@@ -110,10 +113,12 @@ int main(int argc, char* argv[]) {
                       << std::endl;
         });
 
+
+    udp::socket* socket = socket_promise.get_future().get();
     for (std::string line; std::getline(std::cin, line);) {
         if ("quit" == line) {
             break;
-        } else {
+        } else if (line.find("send ") == 0) {
             static const std::regex send_regex("send *([^ ]*) *([^ ]*) *(.*)");
             std::string address;
             std::string port;
@@ -142,7 +147,48 @@ int main(int argc, char* argv[]) {
                       << ":" << endpoint.port()
                       << " message: " << message
                       << std::endl;
-            socket.send_to(asio::buffer(message), endpoint);
+            socket->send_to(asio::buffer(message), endpoint);
+        } else if (line.find("stun ") == 0) {
+            static const std::regex send_regex("stun *([^ ]*) *([^ ]*)$");
+            std::string address;
+            std::string port;
+
+            using regex_iterator = std::regex_token_iterator<std::string::iterator>;
+            auto counter = 0;
+            for (regex_iterator it { std::begin(line), std::end(line), send_regex, {1, 2} }, end;
+                 it != end; ++it, ++counter) {
+                switch (counter) {
+                    case 0: address = *it; break;
+                    case 1: port = *it; break;
+                    default: break;
+                }
+            }
+
+            if (address.empty() || port.empty()) {
+                std::cout << "invalid command format" << std::endl;
+                continue;
+            }
+
+            udp::endpoint endpoint(asio::ip::address::from_string(address), std::stoi(port));
+            uint8_t tsx_id[12] = {0};
+            stun::message msg {stun::message::binding_request, tsx_id};
+
+            // Add a SOFTWARE attribute
+            msg << stun::attribute::software("integra");
+
+            // Add a PRIORITY attribute
+            msg << stun::attribute::priority(0x6e0001fful);
+
+            // Add a ICE-CONTROLLED attribute
+            msg << stun::attribute::ice_controlled(0x932ff9b151263b36ull);
+
+            // Appends a FINGERPRINT attribute as last attribute
+            msg << stun::attribute::fingerprint();
+
+            // Now, send the message
+            socket->send_to(asio::buffer(msg.data(), msg.size()), endpoint);
+        } else {
+            std::cout << "unsupported command" << std::endl;
         }
     }
 
